a7a9093c0c8b12f505d3b257d2922e01a1876a85
alvarez.anais11 <alvarez.anais11@gmail.com>
2018-03-20 20:49:38 -0400

    First JavaScript in-class assignment

--- week-6/in-class/js/learning.js
+++ week-6/in-class/js/learning.js
@@ -1,2 +1,51 @@
 // "use strict" tells the browser to enforce some rules about what can be in our JavaScript.
 "use strict";
+
+let my_name = "Anais";
+let your_name = "Kylo";
+
+let name = prompt("Give us a name: ")
+
+if (name.length === my_name.length){
+  renderOutput("Are you Anais?");
+} else if (name.length === your_name.length) {
+  renderOutput("Are you Kylo?");
+} else {
+  renderOutput("Who are you?");
+}
+
+if (name === my_name) {
+  renderOutput("That's me!");
+} else if (name === your_name) {
+  renderOutput("That's Kylo!");
+} else {
+  renderOutput("I must be someone else.");
+}
+
+/*for (var index = 0; index < 10; index++){
+  renderOutput(index);
+}*/
+
+function helloWorld() {
+  renderOutput("Hello World");
+}
+
+helloWorld();
+
+function helloName(name) {
+  renderOutput("Hello " + name);
+}
+
+helloName(name);
+helloName("Ben");
+helloName("George");
+helloName("Martha");
+
+function todaysHours(days_hours) {
+  renderOutput("ran todaysHours once");
+  renderOutput(days_hours);
+}
+
+//todaysHours("Tuesday: 9a - 5p");
+
+hours.forEach(todaysHours);
